82671aad9ad60c414a809d89277513a05239a185
GrantK <grantklees108@gmail.com>
2017-12-08 11:10:39 -0800

    Restarted My Work after Sos' Lesson
    look at class notes for help!

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -1,50 +1,37 @@
 import java.awt.geom.*;     // for Point2D.Double
 import java.util.ArrayList; // for ArrayList
-import gpdraw.*;            // for DrawingTool
+// import gpdraw.*;            // for DrawingTool
 
 public class IrregularPolygon{
    private ArrayList <Point2D.Double> myPolygon = new ArrayList <Point2D.Double>();
-   private SketchPad myPaper;
-   private DrawingTool myPen;
+   int counter;
    // constructors
    public IrregularPolygon() 
    {
-       myPolygon.add((new Point2D.Double(2.0, 1.0)));
-       myPolygon.add((new Point2D.Double(50.0, 1.0)));
-       myPaper = new SketchPad(500,500);
-       myPen = new DrawingTool(myPaper);
-       //        Point2D.Double from = new Point2D.Double(70, 20);
-       //        Point2D.Double from = new Point2D.Double(50, 50);
-       //        Point2D.Double from = new Point2D.Double(0, 40);
+       myPolygon.add((new Point2D.Double(20.0, 10.0)));
+       myPolygon.add((new Point2D.Double(70.0, 20.0)));
+       myPolygon.add((new Point2D.Double(50.0, 50.0)));
+       myPolygon.add((new Point2D.Double(0.0, 40.0)));
+       for(Point2D.Double pt : myPolygon)
+       {
+           counter += 1;
+           System.out.println(counter + " X:" + pt.getX() + " Y:" + pt.getY());
+       }
    }
 
    // public methods
    public void add(Point2D.Double aPoint) 
    {
-       myPolygon.add(aPoint);
    }
 
    public void draw() 
    {
-       myPen.up();
-       myPen.move(myPolygon.get(0).getX(), myPolygon.get(0).getY());
-       myPen.down();
- 
-       for(int i = 1; i < myPolygon.size(); i++)
-       {
-           myPen.move(myPolygon.get(i).getX(), myPolygon.get(i).getY());
-       }
    }
 
    public double perimeter() 
    {
-       double perim = 0;
-       for(int i = 0; i < myPolygon.size() - 1; i++)
-       {
-           perim += ((Point2D.Double)myPolygon.get(i)).distance((Point2D.Double)myPolygon.get(i + 1));
-       }
-       return perim;
-    }
+       return 0.0;
+   }
 
    public double area() 
    {
