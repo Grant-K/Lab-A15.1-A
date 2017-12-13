6d9e8635fe13d441fb877f025b78dfe35e50edfb
GrantK <grantklees108@gmail.com>
2017-12-13 13:54:03 -0800

    Draw and Perm
    Finished Code for drawin polygons and getting their perimeter.

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -7,30 +7,46 @@ public class IrregularPolygon{
    // constructors
    public IrregularPolygon() 
    {
-       myPolygon.add((new Point2D.Double(20.0, 10.0)));
-       myPolygon.add((new Point2D.Double(70.0, 20.0)));
-       myPolygon.add((new Point2D.Double(50.0, 50.0)));
-       myPolygon.add((new Point2D.Double(0.0, 40.0)));
    }
 
    // public methods
    public void add(Point2D.Double aPoint) 
    {
+        myPolygon.add(new Point2D.Double(aPoint.getX(),  aPoint.getY()));
    }
 
    public void draw() 
    {
        SketchPad myPaper = new SketchPad(500, 500);
        DrawingTool myPencil = new DrawingTool(myPaper);
+       myPencil.down();
+       myPencil.move((myPolygon.get(0)).getX(),(myPolygon.get(0)).getY());
+       for(int i = 0; i < myPolygon.size(); i++)
+       {
+           myPencil.down();
+           myPencil.move((myPolygon.get(i)).getX(),(myPolygon.get(i)).getY());
+       }
+       myPencil.move((myPolygon.get(0)).getX(),(myPolygon.get(0)).getY());
    }
 
    public double perimeter() 
    {
-       return 0.0;
+       double perm = 0;
+        for(int i = 1; i < myPolygon.size(); i++)
+       {
+           perm += myPolygon.get(i).distance(myPolygon.get(i-1));
+       }
+       perm += myPolygon.get(0).distance(myPolygon.get(myPolygon.size()-1));
+       return perm;
    }
 
    public double area() 
    {
        return 0.0;
+   }
+   
+   public String getName() 
+   {
+       return "Grant Klees";
    }
 }
